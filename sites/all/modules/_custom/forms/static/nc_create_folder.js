1857708ed16ea055e125d5be85c93ee4b115a0b5
Ritesh Reddy Samala <RiteshSamala@riteshs-mbp.mobile.asu.edu>
2017-10-03 14:22:15 -0700

    create folder form ajaxification changes, new module ajax_calls added

--- sites/all/modules/_custom/forms/static/nc_create_folder.js
+++ sites/all/modules/_custom/forms/static/nc_create_folder.js
@@ -1,66 +1,39 @@
-jQuery(document).ready(function($) {
-	$('#create_nc_folder').click(function(e){
-		e.preventDefault();
-		// make sure folder name is not empty and also the folder with same name exists
-		var folder_name = $('#create_folder_fname').val();
-		var owner=$('#dragoon_ncCFolder_form input[name=owner]').val();
-		console.log("form check parameters", folder_name, owner);
-		checkValidity(folder_name,owner);
-	});
+(function ($) {
+	Drupal.behaviors.validateCFForm = { 
+		attach: function (context ,settings) { 
+			var form = $("#dragoon_ncCFolder_form", context);
+			form.on('submit', function(e){
+					e.preventDefault();
+					// Run js based validations 
+					var folder_name = $('#create_folder_fname').val();
+					
+					// folder name empty case
+					if(isNameEmpty(folder_name)){
+						showErrorTextbox("create_folder_fname","Empty value");
+						return;
+					}
 
-	function checkValidity(folder_name,owner){
-		//empty value case
-		if(isNameEmpty(folder_name)){
-			showErrorTextbox("create_folder_fname","Empty value");
-			return;
+					//length exceeded case handled with maxlength attribute
+
+					//special characters case
+					if(checkSpecialChars(folder_name)){
+						showErrorTextbox("create_folder_fname","special characters not allowed");
+						return;
+					}
+
+					// If success, trigger event on button
+					// this trigger makes an ajax call, verifies duplicate case and finally creates folder
+					$('#create_nc_folder').trigger('createFolderEvent');
+				});
 		}
-
-		//length exceeded case handled with maxlength attribute
-
-		//special characters case
-		if(checkSpecialChars(folder_name)){
-			showErrorTextbox("create_folder_fname","special characters not allowed");
-			return;
-		}
-
-		//duplicate folder case
-		var folder_id = folder_name+"-"+owner;
-		var req_type = 'check Folder';
-		$.ajax({
-			type: "POST",
-			url: "sites/all/modules/_custom/NC_models/static/nonClassUpdates.php",
-			data: {'folder_id': folder_id, 'req_type': req_type},
-			success: function (data) {
-				if(data==1) {
-					console.log(data);
-					createFolder();
-				}
-				else {
-					console.log(data);
-					showErrorTextbox("create_folder_fname","Folder with given name already exists, try a new one!");
-				}
-			},
-			error: function (data) {
-				console.log(data);
-				console.log("fail");
-			}
-		});
 	}
 
-	function createFolder(){
-		console.log("creating Folder");
-		$.ajax({
-			type: "POST",
-			url: "sites/all/modules/_custom/NC_models/static/nonClassUpdates.php",
-			data: $('#dragoon_ncCFolder_form').serialize(),
-			success: function (data) {
-				console.log(data);
-				location.reload();
-			},
-			error: function (data) {
-				console.log("fail");
-			}
-		});
+	//commands can be returned back from drupal code
+	Drupal.ajax.prototype.commands.reloadPage = function(){
+		//Print success message and reload page
+		$('#block-forms-create-non-class-folder').html('<b>Success!</b>');
+		setTimeout(function(){
+			location.reload();
+		},2000); 
 	}
-
-});
+})(jQuery);
